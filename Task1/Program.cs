22656466a0edf0856c4c8de5e93bf58691cb294a
David Bakalov <davidbakalov33@gmail.com>
2023-02-13 23:27:27 +0200

    1 and 2 are ready

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+var variable = "2 + 2 * 2";
+char[] operators = new char[] { '+', '-', '/', '*' };
 
-Console.WriteLine("Hello, World!");
+List<string> operations = new List<string>();
+
+var buff = "";
+char? oper = null;
+
+foreach (var ch in variable)
+{
+    if (Char.IsDigit(ch))
+    {
+        buff += ch;
+    }
+    else if (operators.Contains(ch))
+    {
+        operations.Add(buff);
+        buff = "";
+        if (oper is not null)
+        {
+            operations.Add(oper.ToString());
+        }
+        oper = ch;
+    }
+}
+
+if (buff != "")
+{
+    operations.Add(buff);
+    operations.Add(oper.ToString());
+}
+Stack<char> calculations= new Stack<char>();
+
+foreach (var operation in operations)
+{
+    var l = Convert.ToChar(operation);
+    if (Char.IsDigit(l))
+    {
+        
+        calculations.Push(l);
+    }
+    else if (operators.Contains(l))
+    {
+        var first=calculations.Pop();
+        var second = calculations.Pop();
+       int firstI = Convert.ToInt32(first);
+       int secondI = Convert.ToInt32(second);
+
+
+       if (l=='+')
+       {
+           int s = firstI + secondI;
+           calculations.Push(Convert.ToChar(s));
+       }else if (l=='-')
+       {
+           int s = firstI - secondI;
+           calculations.Push(Convert.ToChar(s));
+       }else if (l=='*')
+       {
+           int s = firstI * secondI;
+           calculations.Push(Convert.ToChar(s));
+       }else if (l=='/')
+       {
+           int s = firstI / secondI;
+           calculations.Push(Convert.ToChar(s));
+       }
+       
+       
+    }
+   
+}
+
+
+foreach (var VARIABLE in calculations)
+{
+    Console.WriteLine(VARIABLE);
+}
+
+
