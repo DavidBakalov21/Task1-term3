7018031891f9cbea671f70f49ec3d335428dff8a
David Bakalov <davidbakalov33@gmail.com>
2023-02-15 10:40:17 +0200

    almost done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,74 +1,116 @@
-﻿
-var variable = "2 + 2";
-char[] operators = new char[] { '+', '-', '/', '*' };
+﻿var variable = "2 + 2 * 8";
 
+char[] operators = new char[] { '+', '-', '/', '*' };
 List<string> operations = new List<string>();
 
-var buff = "";
-char? oper = null;
-
+var Buffer="";
+char op = ',';
 foreach (var ch in variable)
 {
     if (Char.IsDigit(ch))
     {
-        buff += ch;
+        Buffer += ch;
+        
     }
     else if (operators.Contains(ch))
     {
-        operations.Add(buff);
-        buff = "";
-        if (oper is not null)
-        {
-            operations.Add(oper.ToString());
-        }
-        oper = ch;
-    }
-}
 
-if (buff != "")
-{
-    operations.Add(buff);
-    operations.Add(oper.ToString());
-}
-Stack<char> calculations= new Stack<char>();
 
-foreach (var operation in operations)
-{
-    var l = Convert.ToChar(operation);
-    if (Char.IsDigit(l))
-    {
-        
-        calculations.Push(l);
-    }
-    else if (operators.Contains(l))
-    {
-        var first=calculations.Pop();
-        var second = calculations.Pop();
-       int firstI = int.Parse(first.ToString());
-       int secondI = int.Parse(second.ToString());
-       // How can 2 be converted to 50?
-       
-        
-Console.WriteLine(firstI);
-Console.WriteLine(second);
+        operations.Add(Buffer);
 
-if (l=='+')
-{
-    int s = firstI + secondI;
-    var b = Convert.ToChar(s);
-    calculations.Push(b);
-    
-    
-}
-       
-       
+
+        Buffer = "";
+            if (op!=',')
+            {
+                operations.Add(op.ToString());
+            }
+
+            //Console.WriteLine(Buffer);
+        op = ch;
     }
+
    
+    
 }
+if (Buffer!="")
+{
+    operations.Add(Buffer);
+    operations.Add(op.ToString());
+}
+
+//foreach (var g in operations)
+//{
+  //  Console.WriteLine(g);
+//}
+
+var controll = "?";
+Stack<String> calculations= new Stack<String>();
+foreach (var l in operations)
+{
+    foreach (var c in l.ToCharArray())
+    {
+        if (Char.IsDigit(c))
+        {
+            controll = "Digit";
+        }
+        else if (operators.Contains(c))
+        {
+            controll = "Op";
+        }
+        {
+            
+        }
+    }
+    
+     if (controll=="Digit")
+     {
+         calculations.Push(l);
+     }
+     else if (controll=="Op")
+     {
+         var first=calculations.Pop();
+         var second = calculations.Pop();
+         int firstI = int.Parse(first.ToString());
+         int secondI = int.Parse(second.ToString());
+        // How can 2 be converted to 50?
+       
+        
+        Console.WriteLine(firstI);
+        Console.WriteLine(second);
+
+        if (l=="+")
+        {
+             int s = firstI + secondI;
+             
+             var b = Convert.ToString(s);
+             calculations.Push(b);
+     
+     
+         }else if (l=="-")
+        {
+            int s = firstI - secondI;
+            var b = Convert.ToString(s);
+            calculations.Push(b);   
+        }
+        else if (l=="*")
+        {
+            int s = firstI * secondI;
+            var b = Convert.ToString(s);
+            calculations.Push(b);  
+        }else if(l=="/")
+        {
+            int s = firstI / secondI;
+            var b = Convert.ToString(s);
+            calculations.Push(b);  
+        }
+        
+        
+     }
+    
+ }
 
 
 foreach (var VARIABLE in calculations)
 {
     Console.WriteLine(VARIABLE);
 }
-
