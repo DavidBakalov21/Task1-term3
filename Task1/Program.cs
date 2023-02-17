2006fddf1034aee8d96a1a6e1f751e5386dcf071
David Bakalov <davidbakalov33@gmail.com>
2023-02-17 11:35:52 +0200

    gg

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,61 @@
-﻿var variable = "2+4*6";
+﻿
+bool IsDigit(string? l)
+{
+    if (l is null)
+    {
+        return false;
+    }
+    foreach (var e in l)
+    {
+        if (!Char.IsDigit(e))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool ContainsH(string? l)
+{
+    char[] operatorsHigh = new char[] { '/', '*' };
+    if (l is null)
+    {
+        return false;
+    }
+    foreach (var e in l)
+    {
+        if (!operatorsHigh.Contains(e))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool ContainsL(string? l)
+{
+    if (l is null)
+    {
+        return false;
+    }
+    char[] operatorsLow = new char[] { '-', '+' };
+    foreach (var e in l)
+    {
+        if (!operatorsLow.Contains(e))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+var variable = "2+4*6";
 
 char[] operators = new char[] { '+', '-', '/', '*' };
-List<string> operations = new List<string>();
+var operations = new ArrayList();
 
 var Buffer="";
 char op = ',';
@@ -38,32 +92,33 @@ if (Buffer!="")
     operations.Add(op.ToString());
 }
 
-foreach (var g in operations)
+for (var i = 0; i < operations.Count(); i++)
 {
-    Console.WriteLine(g);
+    Console.WriteLine(operations.GetAt(i));
 }
 Console.WriteLine("---------------");
 
+
 Queue<String> calculations= new Queue<String>();
-Stack<String> oper = new Stack<string>();
-Stack<String> final = new Stack<string>();
+var oper = new Stack();
+var final = new Stack();
 char[] operatorsHigh = new char[] { '/', '*' };
 char[] operatorsLow = new char[] { '-', '+' };
 string[] kk = new string[2];
-foreach (var t in operations)
+for (var t = 0; t < operations.Count(); t++)
 {
     // 
-    foreach (var c in t.ToCharArray())
+    foreach (var c in operations.GetElements())
     {
-        if (Char.IsDigit(c))
+        if (IsDigit(c))
         {
-           calculations.Enqueue(c.ToString());
+           calculations.Enqueue(c);
         }
-       else if (operatorsLow.Contains(c))
+       else if (ContainsL(c))
         {
             if (oper.Count()==0)
             {
-                oper.Push(c.ToString());
+                oper.Push(c);
                
             }
             else if (oper.Count()>0)
@@ -71,7 +126,7 @@ foreach (var t in operations)
                 for (int i = 0; i < oper.Count(); i++)
                 {
                     kk[i] = oper.Pop();
-                    Console.WriteLine(kk[i]);
+                    
                     if (kk[i]!=null)
                     {
                        calculations.Enqueue(kk[i]); 
@@ -80,25 +135,21 @@ foreach (var t in operations)
                     
                     
                 }
-                oper.Push(c.ToString());
+                oper.Push(c);
             }
             
             
-        } else if (operatorsHigh.Contains(c))
+        } else if (ContainsH(c))
         {
-            if (oper.Count==0)
+            if (oper.Count()==0)
             {
                 
-                oper.Push(c.ToString());
-               
-                    
-                    
-                    
-                }
+                oper.Push(c);
+            }
 
             if (oper.Count()>0 &&  oper.Contains(operatorsLow.ToString()))
             {
-                oper.Push(c.ToString());  
+                oper.Push(c);  
             }
           
             else if (oper.Count()>0 &&  oper.Contains(operatorsHigh.ToString()))
@@ -106,7 +157,7 @@ foreach (var t in operations)
                 for (int i = 0; i < oper.Count(); i++)
                 {
                     kk[i] = oper.Pop();
-                    Console.WriteLine(kk[i]);
+                    
                     if (kk[i]!=null)
                     {
                         calculations.Enqueue(kk[i]); 
@@ -115,7 +166,7 @@ foreach (var t in operations)
                     
                     
                 }
-                oper.Push(c.ToString());
+                oper.Push(c);
             }
            
         }
@@ -127,96 +178,39 @@ foreach (var g in calculations)
 {
   Console.WriteLine(g);
 }
-//&& 
- 
-/*foreach (var l in operations)
-{
-    foreach (var c in l.ToCharArray())
-    {
-        if (Char.IsDigit(c))
-        {
-            controll = "Digit";
-        }
-        else if (operators.Contains(c))
-        {
-            controll = "Op";
-        }
-        {
-            
-        }
-    }
-    
-     if (controll=="Digit")
-     {
-         calculations.Push(l);
-     }
-     else if (controll=="Op")
-     {
-         var first=calculations.Pop();
-         var second = calculations.Pop();
-         int firstI = int.Parse(first.ToString());
-         int secondI = int.Parse(second.ToString());
-        // How can 2 be converted to 50?
-       
-        
-        Console.WriteLine(firstI);
-        Console.WriteLine(second);
-
-        if (l=="+")
-        {
-             int s = firstI + secondI;
-             
-             var b = Convert.ToString(s);
-             calculations.Push(b);
-     
-     
-         }else if (l=="-")
-        {
-            int s = firstI - secondI;
-            var b = Convert.ToString(s);
-            calculations.Push(b);   
-        }
-        else if (l=="*")
-        {
-            int s = firstI * secondI;
-            var b = Convert.ToString(s);
-            calculations.Push(b);  
-        }else if(l=="/")
-        {
-            int s = firstI / secondI;
-            var b = Convert.ToString(s);
-            calculations.Push(b);  
-        }
-        
-        
-     }
-    
- }
 
 
 
 
-foreach (var VARIABLE in calculations)
-{
-    Console.WriteLine(VARIABLE);
-}
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 public class ArrayList
 {
-    private int[] _array = new int[10];
+    private string[] _array = new String[10];
 
     private int _pointer = 0;
 
-    public void Add(int element)
+    public void Add(string element)
     {
         _array[_pointer] = element;
         _pointer += 1;
 
         if (_pointer == _array.Length)
         {
-            var extendedArray = new int[_array.Length * 2];
+            var extendedArray = new String[_array.Length * 2];
             for (var i = 0; i < _array.Length; i++)
             {
                 extendedArray[i] = _array[i];
@@ -228,7 +222,7 @@ public class ArrayList
         }
     }
 
-    public void Remove(int element)
+    public void Remove(string element)
     {
         for (var i = 0; i < _pointer; i++)
         {
@@ -245,12 +239,12 @@ public class ArrayList
         }
     }
 
-    public int GetAt(int index)
+    public string GetAt(int index)
     {
         return _array[index];
     }
-
-    public int IndexOf(int element)
+    
+    public int IndexOf(string element)
     {
         for (var i = 0; i < _array.Length; i++)
         {
@@ -263,16 +257,28 @@ public class ArrayList
         return -1;
     }
 
-    public bool Contains(int element)
+    public bool Contains(string element)
     {
         return IndexOf(element) != -1;
     }
-
+    public string[] GetElements()
+    {
+        return _array;
+        // string[] elments = new string[10]; 
+        // for (int i = 0; i < _array.Count(); i++)
+        // {
+        //     elments[i] = _array[i];
+        //     
+        // }
+        // return elments.ToString();
+        //
+    }
     public int Count()
     {
         return _pointer;
     }
 }
+
 
 
 
@@ -295,18 +301,47 @@ public class Stack
         _array[_pointer] = value;
         _pointer++;
     }
-
-    public string Pull()
+    public string GetAt(int index)
+    {
+        return _array[index];
+    }
+    public string Pop()
     {
         if (_pointer == 0)
         {
             //you can also raise an exception here, but we're simple returning nothing
             return null;
         }
-
-        var value = _array[_pointer];
+//original variant (var value = _array[_pointer];) didn't work
+        var value = _array[_pointer-1];
+        //original variant didn't work
         _pointer--;
         return value;
     }
+    public int Count()
+    {
+        return _pointer;
+    }
+    
+    
+    public int IndexOf(string element)
+    {
+        for (var i = 0; i < _array.Length; i++)
+        {
+            if (_array[i] == element)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public bool Contains(string element)
+    {
+        return IndexOf(element) != -1;
+    }
+    
 }
+
 
--- Task1/rewrite.cs
+++ Task1/rewrite.cs
@@ -1,1 +1,74 @@
-﻿
+﻿//&& 
+ 
+/*foreach (var l in operations)
+{
+    foreach (var c in l.ToCharArray())
+    {
+        if (Char.IsDigit(c))
+        {
+            controll = "Digit";
+        }
+        else if (operators.Contains(c))
+        {
+            controll = "Op";
+        }
+        {
+            
+        }
+    }
+    
+     if (controll=="Digit")
+     {
+         calculations.Push(l);
+     }
+     else if (controll=="Op")
+     {
+         var first=calculations.Pop();
+         var second = calculations.Pop();
+         int firstI = int.Parse(first.ToString());
+         int secondI = int.Parse(second.ToString());
+        // How can 2 be converted to 50?
+       
+        
+        Console.WriteLine(firstI);
+        Console.WriteLine(second);
+
+        if (l=="+")
+        {
+             int s = firstI + secondI;
+             
+             var b = Convert.ToString(s);
+             calculations.Push(b);
+     
+     
+         }else if (l=="-")
+        {
+            int s = firstI - secondI;
+            var b = Convert.ToString(s);
+            calculations.Push(b);   
+        }
+        else if (l=="*")
+        {
+            int s = firstI * secondI;
+            var b = Convert.ToString(s);
+            calculations.Push(b);  
+        }else if(l=="/")
+        {
+            int s = firstI / secondI;
+            var b = Convert.ToString(s);
+            calculations.Push(b);  
+        }
+        
+        
+     }
+    
+ }
+
+
+
+
+foreach (var VARIABLE in calculations)
+{
+    Console.WriteLine(VARIABLE);
+}
+*/
