81a138812d71fce2149c9e14922ba5afc216662c
David Bakalov <davidbakalov33@gmail.com>
2023-02-22 10:11:40 +0200

    fff

---
+++ Task1/Queue.cs
@@ -0,0 +1,79 @@
+﻿namespace Task1;
+
+public class Queue
+{
+    private const int Capacity = 50;
+
+    private string[] _array = new string[Capacity];
+
+    private int _pointer;
+    public Array GetArray()
+    {
+        return _array;
+    }
+   
+    public ArrayList toList(Queue Q)
+    {
+        var Converted = new ArrayList();
+        for (var  elToConvert=0; elToConvert<Q.Count(); elToConvert++)
+        {
+            Converted.Add(Q.GetAt(elToConvert));
+        }
+
+        return Converted;
+    }
+    public void Enqueue(string value)
+    {
+        _array[_pointer] = value;
+        _pointer++;
+    }
+    public string GetAt(int index)
+    {
+        return _array[index];
+    }
+    public string Dequeue()
+    {
+       
+//original variant (var value = _array[_pointer];) didn't work
+        var value = _array[0];
+        //original variant didn't work
+        for (var i = 0; i < _pointer; i++)
+        {
+            if (i == 0)
+            {
+                for (var j = i; j < _pointer - 1; j++)
+                {
+                    _array[j] = _array[j + 1];
+                }
+
+                _pointer -= 1;
+                
+            }
+        }
+        return value;
+    }
+    public int Count()
+    {
+        return _pointer;
+    }
+    
+    
+    public int IndexOf(string element)
+    {
+        for (var i = 0; i < _array.Length; i++)
+        {
+            if (_array[i] == element)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public bool Contains(string element)
+    {
+        return IndexOf(element) != -1;
+    }
+    
+}
---
+++ Task1/Stack.cs
@@ -0,0 +1,71 @@
+﻿namespace Task1;
+
+public class Stack
+{
+    private const int Capacity = 50;
+
+    private string[] _array = new string[Capacity];
+
+    private int _pointer;
+
+    public void Push(string value)
+    {
+        
+
+        _array[_pointer] = value;
+        _pointer++;
+    }
+    public string GetAt(int index)
+    {
+        return _array[index];
+    }
+    public string Pop()
+    {
+        if (_pointer == 0)
+        {
+            //you can also raise an exception here, but we're simple returning nothing
+            return null;
+        }
+//original variant (var value = _array[_pointer];) didn't work
+        var value = _array[_pointer-1];
+        //original variant didn't work
+        _pointer--;
+        return value;
+    }
+    public int Count()
+    {
+        return _pointer;
+    }
+
+    public string Peek()
+    {
+        if (_pointer == 0)
+        {
+            //you can also raise an exception here, but we're simple returning nothing
+            return null;
+        }
+        var value = _array[_pointer-1];
+        return value;
+        
+    }
+    
+    public int IndexOf(string element)
+    {
+        for (var i = 0; i < _array.Length; i++)
+        {
+            if (_array[i] == element)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public bool Contains(string element)
+    {
+        return IndexOf(element) != -1;
+    }
+    
+}
+
